492d59f3b9a6d7013dae0c1f1130b02c8db795b7
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 16:12:02 +0100

    completed JSnack 6 bonus exercise

--- snack-6-bonus/script.js
+++ snack-6-bonus/script.js
@@ -7,3 +7,16 @@ dove N è un numero indicato dall’utente. */
 // Chiedo all'utente per quanti numeri vuole il cubo (partendo da 1)
 const indexCubeNumber = prompt("Per quanti numeri (partendo da 1) vuoi il cubo?");
 
+// Creo un array (che inizializzo solo col numero zero) che conterrà tutti gli N numeri al cubo richiesti dall'utente
+const arrayCubeNumber = [0];
+// Ciclo in cui inserisco in un array i primi N numeri al cubo richiesti dall'utente 
+for (let i = 0; i < indexCubeNumber; i++) {
+    // Uso l'operatore di elevazione a potenza (**)
+    arrayCubeNumber[i] = (i + 1) ** 3;
+}
+// Messaggio di output di quanti numeri al cubo ha richiesto l'utente
+console.log("Hai richiesto il cubo dei primi " + indexCubeNumber + " numeri");
+// Stampo a schermo i primi N numeri al cubo richiesti dall'utente
+for (let i = 0; i < arrayCubeNumber.length; i++) {
+    console.log(arrayCubeNumber[i]);
+}
