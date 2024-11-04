750021e81fe148e5cc9a509cedd6a80ae6789f8b
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 14:12:20 +0100

    add emaillist and add prompt for insert email

--- mail/script.js
+++ mail/script.js
@@ -6,3 +6,15 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 Non è necessario provvedere alla validazione delle email */
+
+// Creo una lista di email
+const mailList = [
+    "pippo@gmail.com",
+    "pluto@gmail.com",
+    "paperino@gmail.com",
+    "topolino@tiscali.it",
+    "danilo.mosca@gmail.com",
+]
+
+// Chiedo all'utente di inserire una mail
+const userMail = prompt("Inserisci la tua email");
