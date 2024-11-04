7615ebedd6bd1fa3afec914e62d5e320edca5c54
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 21:16:58 +0100

    add second solution for JSnack 7 bonus exercise

--- snack-7-bonus/script.js
+++ snack-7-bonus/script.js
@@ -1,7 +1,24 @@
 "use strict";
 console.clear();
 
-/* Stampa le potenze di 2 fino a 1000. */
+/* Stampa a schermo le potenze fino al numero massimo 1000 */
+
+// Contatore delle potenze e variabile accumulatore di elevazione a potenza
+let i = 0, pow = 0;
+// Flag di controllo ciclo
+let flag = true;
+while (flag) {
+    pow = 2 ** i;
+    if (pow < 1000) {
+        console.log(`2 alla ${i} = ${pow}`);
+    }
+    else {
+        flag = false;
+    }
+    i++;
+}
+
+/* Oppure stampa a schermo le potenze di 2 fino a 1000. */
 
 // Messaggio di anteprima
 console.log("Potenze di 2 fino a 1000:");
