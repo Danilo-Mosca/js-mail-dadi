8223c0f66f96afea1d5a0687d6e2a014fb687c43
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 15:54:10 +0100

    add variable input for user

---
+++ snack-6-bonus/script.js
@@ -0,0 +1,9 @@
+"use strict";
+console.clear();
+
+/* Stampa il cubo dei primi N numeri,
+dove N è un numero indicato dall’utente. */
+
+// Chiedo all'utente per quanti numeri vuole il cubo (partendo da 1)
+const indexCubeNumber = prompt("Per quanti numeri (partendo da 1) vuoi il cubo?");
+
