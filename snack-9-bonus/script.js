f4585741fa0e12a273fe5faf18ac55457eaf88de
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 17:25:16 +0100

    add loop, average and sum variables for first 10 numbers. JSnack 9 bonus exercise complete

--- snack-9-bonus/script.js
+++ snack-9-bonus/script.js
@@ -2,3 +2,16 @@
 console.clear()
 
 /* Calcola la somma e la media dei primi 10 numeri. */
+
+// Creo la variabile somma e la inizializzo a zero
+let sum = 0;
+// Creo il ciclo che andrà a sommare i primi 10 numeri e la loro media
+for (let i = 0; i <= 10; i++) {
+    sum += i;
+}
+// Stampo a schermo la somma
+console.log("La somma dei primi 10 numeri è: " + sum);
+
+// Creo la variabile che conterra la media
+const average = sum / 10;
+console.log("La media dei primi 10 numeri interi è: " + average);
