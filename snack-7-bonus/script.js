4725c9ce78f0e7dfca43a2e7e4f9e20db3c86375
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 16:25:54 +0100

    add variable input for user

--- snack-7-bonus/script.js
+++ snack-7-bonus/script.js
@@ -2,3 +2,12 @@
 console.clear();
 
 /* Stampa le potenze di 2 fino a 1000. */
+
+// Messaggio di anteprima
+console.log("Potenze di 2 fino a 1000:");
+// Variabile accumulatore
+let result = 0;
+// Ciclo che stampa le potenze di due
+for (let i = 0; i < 1000; i++) {
+
+}
