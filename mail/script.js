70c58dc81cf92a0099e3f93400f08132e4e956e3
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 14:06:48 +0100

    add folder and file for Mail exercise

---
+++ mail/script.js
@@ -0,0 +1,8 @@
+"use strict";
+console.clear();
+
+/* Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email */
