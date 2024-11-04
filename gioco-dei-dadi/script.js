85d3742ffd86f453a11a77f47b1f32e312295a62
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 14:35:34 +0100

    add function for random numbers dice generator

--- gioco-dei-dadi/script.js
+++ gioco-dei-dadi/script.js
@@ -4,3 +4,9 @@ console.clear();
 /* Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+// Creo la funzione che genera un numero casuale compreso tra "min" e "max" che nel caso dei dadi è
+// un numero compreso fra 1 e 6
+function randomDice (max. min) {
+    return Math.floor((Math.random() * (max - min)) + 1) + min;
+}
