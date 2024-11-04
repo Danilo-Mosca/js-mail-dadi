62b2ad4b72bd3b2b64ccc5625a964701de0ad9fc
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-11-04 14:39:48 +0100

    add variable for dice numbers user and pc

--- gioco-dei-dadi/script.js
+++ gioco-dei-dadi/script.js
@@ -5,8 +5,14 @@ console.clear();
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
 
+const randomPCnumber = randomDice(1, 6);
+const randomUserNumber = randomDice(1,6);
+// Output di prova
+console.log(randomPCnumber, randomUserNumber);
+
+
 // Creo la funzione che genera un numero casuale compreso tra "min" e "max" che nel caso dei dadi è
 // un numero compreso fra 1 e 6
-function randomDice (max. min) {
+function randomDice (min, max) {
     return Math.floor((Math.random() * (max - min)) + 1) + min;
 }
